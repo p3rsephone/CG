8c974e5be5fc8b1a27f10921bc263e8b76837f2c
Tânia Silva <tdosilva@gmail.com>
2018-02-26 21:43:38 +0000

    Add box parsing

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -3,15 +3,25 @@
 #include <string>
 #include <stdio.h>
 #include <string.h>
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#else
 #include <GL/gl.h>
+#endif
 
 using namespace std;
 
 //Importante!!!
 //Compilar com o seguinte exemplo: g++ -std=c++11 -o generator_test main.cpp
 //Apenas o C++11 contém stoi's e stod's por isso temos de limitar a versão
-//TODO: Mudar os ints para floats
+//TODO: Change ints to floats
 
+/**
+    Shows the usage of this file.
+ */
+void usage(){
+    // TODO: Write instructions
+}
 
 int addToXML(){
 
@@ -21,7 +31,7 @@ int addToXML(){
 int parsePlane(GLint size, string filename){
     ofstream file (filename);
     if (size<=0) {
-        cout << "Size must be positive." << endl
+        cout << "Size must be positive." << endl;
         return 1;
     }
 
@@ -40,7 +50,13 @@ int parsePlane(GLint size, string filename){
     return 0;
 }
 
-int parseBox(GLint x, GLint y, GLint z, string filename){
+int parseBox(GLint x, GLint y, GLint z, GLint d, string filename){
+
+    cout << "X: " << x << endl
+         << "Y: " << y << endl
+         << "Z: " << z << endl
+         << "Div: " << d << endl
+         << "Filename: " << filename << endl;
 
     return 0;
 }
@@ -70,6 +86,7 @@ int main(int argc, char* argv[]) {
 
     if(argc <= 1){
         cout << "Command needs more arguments" << endl;
+        usage();
     }
 
     //*
@@ -79,21 +96,28 @@ int main(int argc, char* argv[]) {
         if(argc == 4) {
             GLint size = (GLint) stoi(argv[2]);
             string filename = argv[3];
+            
             parsePlane(size,filename);
+        } else {
+            usage();
         }
     }
 
     //*
     // Verifies if the object is a box
     // Example: generator box 1 1 1 box.3d
+    // Example 2: generator box 1 1 1 3 box.3d
     if(strcmp(argv[1],"box") == 0){
-        if(argc == 6) {
+        if(argc == 6 || argc == 7) {
             GLint x = (GLint) stoi(argv[2]);
             GLint y = (GLint) stoi(argv[3]);
             GLint z = (GLint) stoi(argv[4]);
-            string filename = argv[5];
+            GLint d = (argc == 7) ? stoi(argv[5]) : 1;
+            string filename = (argc == 7) ? argv[6] : argv[5];
 
-            parseBox(x, y, z, filename);
+            parseBox(x, y, z, d, filename);
+        } else {
+            usage();
         }
     }
 
@@ -103,12 +127,14 @@ int main(int argc, char* argv[]) {
     if(strcmp(argv[1],"sphere") == 0){
         if(argc == 6){
 
-            GLdouble radius = (GLdouble)stod(argv[2]);
-            GLint slices = (GLint)stoi(argv[3]);
-            GLint stacks = (GLint)stoi(argv[4]);
+            GLdouble radius = (GLdouble) stod(argv[2]);
+            GLint slices = (GLint) stoi(argv[3]);
+            GLint stacks = (GLint) stoi(argv[4]);
             string filename = argv[5];
 
             parseSphere(radius, slices, stacks, filename);
+        } else {
+            usage();
         }
     }
 
@@ -117,13 +143,15 @@ int main(int argc, char* argv[]) {
     // Example: generator cone 1 1 20 20 cone.3d
     if(strcmp(argv[1],"cone") == 0){
         if(argc == 7){
-            GLdouble base = (GLdouble)stod(argv[2]);
-            GLdouble height = (GLdouble)stod(argv[3]);
-            GLint slices = (GLint)stoi(argv[4]);
-            GLint stacks = (GLint)stoi(argv[5]);
+            GLdouble base = (GLdouble) stod(argv[2]);
+            GLdouble height = (GLdouble) stod(argv[3]);
+            GLint slices = (GLint) stoi(argv[4]);
+            GLint stacks = (GLint) stoi(argv[5]);
             string filename = argv[6];
 
             parseCone(base, height, slices, stacks, filename);
+        } else {
+            usage();
         }
     }
 
