6365311ab16f824db455e7a7be3138d00ec77890
Bernardo Silva <a77230@alunos.uminho.pt>
2018-03-02 10:51:12 +0000

    finished createSphere, might need tweaks later, after further testing

--- src/generator/figures.cpp
+++ src/generator/figures.cpp
@@ -109,16 +109,57 @@ vector<Point*> createBox(double x, double y, double z, int d){
 
     return points;
 }
+
 /**
- * [createSphere description]
+ * Creates vertice points for a sphere with a certain radius, slices and stacks
  * Usage: generator sphere 1 20 20 sphere.3d
- * @param  radius   [description]
- * @param  slices   [description]
- * @param  stacks   [description]
- * @return          [description]
+ * @param  radius   Radius of the sphere
+ * @param  slices   Number of slices in the sphere
+ * @param  stacks   Number of stacks in the sphere
+ * @return      Vertice Points
  */
 vector<Point*> createSphere(double radius, int slices, int stacks){
     vector<Point*> points;
+
+    double x;
+    double y;
+    double z;
+
+    double fiShift = M_PI_2 / slices;
+    double thetaShift = M_PI_2 / stacks;
+
+    //Using spheric coordinates
+    //x = radius * sin(theta) * cos(fi);
+    //y = radius * sin(theta) * sin(fi);
+    //z = radius * cos(theta);
+    for(double fi = 0; fi < M_PI_2 ; fi+= fiShift){
+        for(double theta = 0; theta < M_PI_2 ; theta += thetaShift){
+            //Triangular ends of the sphere
+            if(theta == 0){
+                points.push_back(new Point(radius * sin(theta) * cos(fi) , radius * sin(theta) * sin(fi) , radius * cos(theta)));
+                points.push_back(new Point(radius * sin(theta+thetaShift) * cos(fi+fiShift) , radius * sin(theta+thetaShift) * sin(fi+fiShift) , radius * cos(theta+thetaShift)));
+                points.push_back(new Point(radius * sin(theta+thetaShift) * cos(fi) , radius * sin(theta+thetaShift) * sin(fi) , radius * cos(theta+thetaShift)));
+            }
+
+            else if(theta == M_PI_2 - thetaShift){
+                points.push_back(new Point(radius * sin(theta) * cos(fi) , radius * sin(theta) * sin(fi) , radius * cos(theta)));
+                points.push_back(new Point(radius * sin(theta) * cos(fi+fiShift) , radius * sin(theta) * sin(fi+fiShift) , radius * cos(theta)));
+                points.push_back(new Point(radius * sin(theta+thetaShift) * cos(fi+fiShift) , radius * sin(theta+thetaShift) * sin(fi+fiShift) , radius * cos(theta+thetaShift)));
+            }
+
+            //Rectangles within the sphere
+            else {
+                points.push_back(new Point(radius * sin(theta) * cos(fi) , radius * sin(theta) * sin(fi) , radius * cos(theta)));
+                points.push_back(new Point(radius * sin(theta+thetaShift) * cos(fi+fiShift) , radius * sin(theta+thetaShift) * sin(fi+fiShift) , radius * cos(theta+thetaShift)));
+                points.push_back(new Point(radius * sin(theta+thetaShift) * cos(fi) , radius * sin(theta+thetaShift) * sin(fi) , radius * cos(theta+thetaShift)));
+
+                points.push_back(new Point(radius * sin(theta) * cos(fi) , radius * sin(theta) * sin(fi) , radius * cos(theta)));
+                points.push_back(new Point(radius * sin(theta) * cos(fi+fiShift) , radius * sin(theta) * sin(fi+fiShift) , radius * cos(theta)));
+                points.push_back(new Point(radius * sin(theta+thetaShift) * cos(fi+fiShift) , radius * sin(theta+thetaShift) * sin(fi+fiShift) , radius * cos(theta+thetaShift)));
+            }
+
+        }
+    }
 
     return points;
 }
