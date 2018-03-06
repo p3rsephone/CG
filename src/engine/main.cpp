81742cb60f50db00915acdc98fcf4fea5c39f560
a-sac <asergioalvesc@gmail.com>
2018-03-06 22:29:34 +0000

    explorer mode camera done

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -2,6 +2,11 @@
 
 Scene* scene;
 
+float raio = 30;
+float beta = 1;
+float alfa = 1;
+int camera = 0;
+
 void changeSize(int w, int h) {
 
 	// Prevent a divide by zero, when window is too short
@@ -27,6 +32,52 @@ void changeSize(int w, int h) {
 	glMatrixMode(GL_MODELVIEW);
 }
 
+void axis_system(){
+	glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+	glBegin(GL_LINES);
+		glColor3f(1.0,0.0,0.0);
+		glVertex3f(0,0,0);
+		glVertex3f(30,0,0);
+
+		glColor3f(0.0,1.0,0.0);
+		glVertex3f(0,0,0);
+		glVertex3f(0,30,0);
+
+		glColor3f(0.0,1.0,1.0);
+		glVertex3f(0,0,0);
+		glVertex3f(0,0,30);
+	glEnd();
+}
+
+void processKeys(unsigned char key, int xx, int yy) {
+	switch(key){
+		case('a'):
+			alfa-=0.1;
+			break;
+		case('d'):
+			alfa+=0.1;
+			break;
+		case('w'):
+			if(beta < 1.5) beta+=0.1;
+			break;
+		case('s'):
+			if(beta > -1.5) beta-=0.1;
+			break;
+		case('+'):
+			raio+=0.2;
+			break;
+		case('-'):
+			raio-=0.2;
+			break;
+		case('c'):
+			camera++;
+			break;
+		default:
+			break;
+	}
+	glutPostRedisplay();
+}
+
 void renderScene(void) {
 
 	// clear buffers
@@ -34,12 +85,20 @@ void renderScene(void) {
 
 	// set the camera
 	glLoadIdentity();
-	gluLookAt(30.0,30.0,0.0,
+	if(camera%2==0){
+		gluLookAt(raio*cos(beta)*sin(alfa),raio*sin(beta),raio*cos(beta)*cos(alfa), 
 		      0.0,0.0,0.0,
 			  0.0f,1.0f,0.0f);
+	}
+	else{
+		gluLookAt(5,5,5, 
+			  raio*cos(beta)*sin(alfa),raio*sin(beta),raio*cos(beta)*cos(alfa),
+			  0.0f,1.0f,0.0f);
+	}
 //    scene->draw();
     //glutWireTeapot(1);
     scene->draw();
+		axis_system();
 
     glutSwapBuffers();
 }
@@ -59,6 +118,8 @@ void initGL(int argc, char **argv){
   glutReshapeFunc(changeSize);
   glutIdleFunc(renderScene);
 
+// event handler
+	glutKeyboardFunc(processKeys);
 // OpenGL settings
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
