46da5df2b7cb360759148511f15520a72750b442
Bernardo Silva <a77230@alunos.uminho.pt>
2018-03-08 17:55:46 +0000

    Added cylinder

--- src/generator/headers/figures.h
+++ src/generator/headers/figures.h
@@ -11,5 +11,6 @@ vector<Point*> createPlane(double size);
 vector<Point*> createBox(double x, double y, double z, int d);
 vector<Point*> createSphere(double radius, int slices, int stacks);
 vector<Point*> createCone(double base, double height, int slices, int stacks);
+vector<Point*> createCylinder(double r, double height, int slices, int stacks);
 
 #endif
